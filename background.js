5dbea24f4c3838726d22699f413ee89d38e72bd3
Derek <derek.borden.999@gmail.com>
2023-11-23 18:09:10 -0800

    things

--- background.js
+++ background.js
@@ -1,21 +1,31 @@
 // background.js
-let url = urlInput; // the url of the page to open and accept cookies
+let url = ""; // Initialize the url variable
 
 // listen for messages from the popup
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "setUrl") {
     // set the url from the popup input
     url = message.url;
-    sendResponse({status: "OK"});
+    sendResponse({ status: "OK" });
   } else if (message.type === "openUrl") {
     // open the url in a new tab
-    chrome.tabs.create({url: url}, (tab) => {
+    chrome.tabss.create({ url: url }, (tab) => {
       // inject the content script to the tab
       chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ["content.js"]
+        target: { tabId: tab.id },
+        function: () => {
+          // content script code here
+          let cookieConsentElements = document.querySelectorAll("[data-cookie-consent], [data-cookiebanner], [data-nm-cookie-consent], .cookie-consent, .cookie-banner, .cookie-notice, .cookie-policy, .cookie-law, .cookie-message, .cookie-popup, .cookie-alert, .cookie-warning, .cookie-info, .cookie-dialog, .cookie-modal, .cookie-container, .cookie-bar, .cookie-button, .cookie-agree, .cookie-accept, .cookie-close, .cookie-dismiss, .cookie-ok, .cookie-yes, .cookie-no, .cookie-optout, .cookie-opt-in, .cookie-opt-out");
+
+          for (let element of cookieConsentElements) {
+            if (element.tagName === "BUTTON" || element.tagName === "A") {
+              element.click();
+            } else {
+              element.style.display = "none";
+            }
+          }
+        },
       });
     });
   }
 });
-
--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
-
 // popup.js
 // get the elements from the popup
+let urlInput = document.getElementById("url-input");
 let openButton = document.getElementById("open-button");
 
 // send the url to the background script
 function setUrl() {
   let url = urlInput.value;
-  chrome.runtime.sendMessage({type: "setUrl", url: url}, (response) => {
+  chrome.runtime.sendMessage({ type: "setUrl", url: url }, (response) => {
     if (response.status === "OK") {
       console.log("URL set successfully");
     }
@@ -15,17 +15,9 @@ function setUrl() {
 
 // send a message to the background script to open the url
 function openUrl() {
-  chrome.runtime.sendMessage({type: "openUrl"});
+  chrome.runtime.sendMessage({ type: "openUrl" });
 }
 
 // add event listeners to the elements
-window.addEventListener("DOMContentLoaded", (event) => {
-  // get the urlInput element
-  let urlInput = document.getElementById("urlInput");
-  // add event listener to the element
-  urlInput.addEventListener("change", setUrl);
-});
-
+urlInput.addEventListener("input", setUrl); // Using "input" instead of "change" to update on every input change
 openButton.addEventListener("click", openUrl);
-
-

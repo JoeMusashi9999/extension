f4aec8e5ad9e13478328c286a0d3e3f655fbd5bf
Derek <derek.borden.999@gmail.com>
2023-11-23 18:13:24 -0800

    DOM listener

--- popup.js
+++ popup.js
@@ -1,23 +1,25 @@
 // popup.js
-// get the elements from the popup
-let urlInput = document.getElementById("url-input");
-let openButton = document.getElementById("open-button");
+document.addEventListener("DOMContentLoaded", function () {
+  // get the elements from the popup
+  let urlInput = document.getElementById("url-input");
+  let openButton = document.getElementById("open-button");
 
-// send the url to the background script
-function setUrl() {
-  let url = urlInput.value;
-  chrome.runtime.sendMessage({ type: "setUrl", url: url }, (response) => {
-    if (response.status === "OK") {
-      console.log("URL set successfully");
-    }
-  });
-}
+  // send the url to the background script
+  function setUrl() {
+    let url = urlInput.value;
+    chrome.runtime.sendMessage({ type: "setUrl", url: url }, (response) => {
+      if (response.status === "OK") {
+        console.log("URL set successfully");
+      }
+    });
+  }
 
-// send a message to the background script to open the url
-function openUrl() {
-  chrome.runtime.sendMessage({ type: "openUrl" });
-}
+  // send a message to the background script to open the url
+  function openUrl() {
+    chrome.runtime.sendMessage({ type: "openUrl" });
+  }
 
-// add event listeners to the elements
-urlInput.addEventListener("input", setUrl); // Using "input" instead of "change" to update on every input change
-openButton.addEventListener("click", openUrl);
+  // add event listeners to the elements
+  urlInput.addEventListener("input", setUrl);
+  openButton.addEventListener("click", openUrl);
+});

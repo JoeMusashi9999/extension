9a4bcca20e633ce6af193ad24da5fe7ffd3e6446
Derek <derek.borden.999@gmail.com>
2023-11-25 12:33:03 -0800

    link fixing

--- background.js
+++ background.js
@@ -4,7 +4,8 @@ let url = "";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "setUrl") {
-    url = message.url;
+    // Ensure the URL is well-formed
+    url = message.url.startsWith("http") ? message.url : "https://" + message.url;
     sendResponse({ status: "OK" });
   } else if (message.type === "openUrl") {
     chrome.tabs.create({ url: url });

3ed5e64fd70cf9849d06814ab07028fb66dfc1e6
Derek <derek.borden.999@gmail.com>
2023-11-25 12:34:59 -0800

    keydown

--- popup.js
+++ popup.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("URL set successfully");
       }
     });
-    
   }
 
   // send a message to the background script to open the url
@@ -22,5 +21,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // add event listeners to the elements
   urlInput.addEventListener("input", setUrl);
+
+  // Listen for "keydown" event on the input field
+  urlInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      // Prevent the default behavior (form submission, new line in textarea, etc.)
+      event.preventDefault();
+      
+      // Trigger the setUrl function when the "Enter" key is pressed
+      setUrl();
+
+      // Optionally, you can also trigger the openUrl function here if needed
+      // openUrl();
+    }
+  });
+
   openButton.addEventListener("click", openUrl);
 });

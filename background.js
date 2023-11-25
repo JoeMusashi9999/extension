cd1adcf8e75c1f4ad4ed9e1827ffbd78af0b055f
Derek <derek.borden.999@gmail.com>
2023-11-25 12:54:48 -0800

    from file

--- background.js
+++ background.js
@@ -1,13 +1,29 @@
 // background.js
-
 let url = "";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "setUrl") {
-    // Ensure the URL is well-formed
     url = message.url.startsWith("http") ? message.url : "https://" + message.url;
     sendResponse({ status: "OK" });
-  } else if (message.type === "openUrl") {
-    chrome.tabs.create({ url: url });
+  } else if (message.type === "openUrlsFromFile") {
+    openUrlsFromFile();
   }
 });
+
+function openUrlsFromFile() {
+  // Replace 'file.txt' with the path to your text file containing URLs
+  fetch(chrome.extension.getURL('file.txt'))
+    .then(response => response.text())
+    .then(text => {
+      const urls = text.split('\n').map(url => url.trim()).filter(url => url !== '');
+
+      if (urls.length > 0) {
+        urls.forEach(url => {
+          chrome.tabs.create({ url: url });
+        });
+      } else {
+        console.error('No valid URLs found in the file.');
+      }
+    })
+    .catch(error => console.error('Error reading file:', error));
+}
--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
 // popup.js
 document.addEventListener("DOMContentLoaded", function () {
-  // get the elements from the popup
   let urlInput = document.getElementById("url-input");
-  let openButton = document.getElementById("open-button");
+  let openFileButton = document.getElementById("open-file-button");
 
-  // send the url to the background script
   function setUrl() {
     let url = urlInput.value;
     chrome.runtime.sendMessage({ type: "setUrl", url: url }, (response) => {
@@ -14,27 +12,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // send a message to the background script to open the url
-  function openUrl() {
-    chrome.runtime.sendMessage({ type: "openUrl" });
-  }
-
-  // add event listeners to the elements
   urlInput.addEventListener("input", setUrl);
 
-  // Listen for "keydown" event on the input field
   urlInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-      // Prevent the default behavior (form submission, new line in textarea, etc.)
       event.preventDefault();
-      
-      // Trigger the setUrl function when the "Enter" key is pressed
       setUrl();
-
-      // Optionally, you can also trigger the openUrl function here if needed
-      openUrl();
     }
   });
 
-  openButton.addEventListener("click", openUrl);
+  openFileButton.addEventListener("click", openUrlsFromFile);
 });
+
+
